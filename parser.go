3ce972888028969cc1c406c710f62101909c55a5
Seb Ospina <kraige@gmail.com>
2018-10-04 20:06:20 +0200

    Refactoring and changing stuff a bit

--- parser.go
+++ parser.go
@@ -6,48 +6,54 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 	"time"
 )
 
 type sshConnectInfo struct {
-	sourceIP   string
-	targetUser string
+	targetUser map[string]int
 	count      int
-	lastSeen   Time
+	lastSeen   time.Time
 }
 
-func parseFailedPasswordLine(line string) (sshConnectInfo, err) {
-	lineContent := split(line)
-	if lineContent.len() != 9 {
-		return nil, errors.New("number of fields in Failed Password not 9")
+func parseFailedPasswordLine(line string, blacklist map[string]sshConnectInfo) error {
+	lineContent := strings.Split(line, " ")
+	if len(lineContent) != 9 {
+		return errors.New("number of fields in Failed Password not 9")
 	}
-	return sshConnectInfo{
-		sourceIP:   lineContent[6],
-		targetUser: lineContent[4],
-		count:      1,
-		lastSeen:   time.Now(),
-	}, nil
+	sourceIP := lineContent[6]
+	_, ok := blacklist[sourceIP]
+	if ok {
+		blacklist[sourceIP].count++
+	} else {
+		blacklist[lineContent[6]] = sshConnectInfo{
+			targetUser: {lineContent[4]: 1},
+			count:      1,
+			lastSeen:   time.Now(),
+		}
+	}
+	return nil
 }
-func parseSSHLogLine(line string, blacklist map[string]int) err {
-	if strings.startsWith("Failed password for ") {
-		if connectInfo, err = parseFailedPasswordLine(line); err != nil {
+func parseSSHLogLine(line string, blacklist map[string]sshConnectInfo) error {
+	if strings.HasPrefix(line, "Failed password for ") {
+		if err := parseFailedPasswordLine(line, blacklist); err != nil {
 			return err
 		}
 	}
+	return nil
 }
-func readStdIn() {
+func readStdIn(blacklist map[string]sshConnectInfo) {
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		text, err := reader.ReadString('\n')
 		if err != nil {
 			log.Println("error:", err)
 		}
-		parseSSHLogLine(text)
+		parseSSHLogLine(text, blacklist)
 	}
-
 }
 func main() {
-	var blacklist map[string]int
-	readStdIn()
+	var blacklist map[string]sshConnectInfo
+	readStdIn(blacklist)
 	fmt.Println("parser")
 }
--- parser_test.go
+++ parser_test.go
@@ -3,7 +3,13 @@ package main
 import "testing"
 
 func TestParseSSHLogLine(t *testing.T) {
-	var blacklist map[string]int
+	var blacklist map[string]sshConnectInfo
 	logLine := "Failed password for root from 59.144.172.42 port 6721 ssh2\n"
-	assert.Equal(t, blacklist["59.144.172.42"], 1, "Adds simple IP to blacklist")
+	val, ok := blacklist["59.144.172.42"]
+	if !ok {
+		t.Errorf("IP should have been blacklisted")
+	}
+	if val != 1 {
+		t.Errorf("IP count not incremented")
+	}
 }
