239a2c0aebb9811b76107bfa8e31865bc59ba45e
Seb Ospina <kraige@gmail.com>
2018-10-14 14:31:56 +0200

    Adding GeoIP lat/lon for map

--- parser.go
+++ parser.go
@@ -4,10 +4,15 @@ import (
 	"bufio"
 	"errors"
 	"fmt"
+	"io"
 	"log"
+	"math"
+	"net"
 	"os"
 	"strings"
 	"time"
+
+	geoip2 "github.com/oschwald/geoip2-golang"
 )
 
 // connectInfo could potentially be used for other connection attempts, not just SSH
@@ -15,10 +20,12 @@ type connectInfo struct {
 	targetUser map[string]int
 	count      int
 	lastSeen   time.Time
+	latitude   float64
+	longitude  float64
 }
 
 // parseSSHFailedPasswordLine checks SSH failed password and feeds the blacklist struct
-func parseSSHFailedPasswordLine(line string, blacklist map[string]*connectInfo) error {
+func parseSSHFailedPasswordLine(line string, blacklist map[string]*connectInfo, db *geoip2.Reader) error {
 	// Example of error line:
 	// "Failed password for root from WW.XX.YY.ZZ port 6721 ssh2"
 	lineContent := strings.Split(line, " ")
@@ -26,6 +33,10 @@ func parseSSHFailedPasswordLine(line string, blacklist map[string]*connectInfo) 
 		return errors.New("number of fields in Failed Password not 9")
 	}
 	sourceIP := lineContent[5]
+	ip := net.ParseIP(sourceIP)
+	if ip == nil {
+		return errors.New("Invalid sourceIP")
+	}
 	targetUser := lineContent[3]
 	_, ok := blacklist[sourceIP]
 	if ok {
@@ -37,38 +48,91 @@ func parseSSHFailedPasswordLine(line string, blacklist map[string]*connectInfo) 
 			blacklist[sourceIP].targetUser[targetUser] = 1
 		}
 	} else {
+		latitude := -10000.0
+		longitude := -10000.0
+		record, err := db.City(ip)
+		if err == nil {
+			latitude = record.Location.Latitude
+			longitude = record.Location.Longitude
+		}
 		blacklist[sourceIP] = &connectInfo{
 			targetUser: map[string]int{targetUser: 1},
 			count:      1,
 			lastSeen:   time.Now(),
+			latitude:   latitude,
+			longitude:  longitude,
 		}
 	}
 	return nil
 }
 
 // parseSSHLogLine should cover main ssh log lines related to errors.
-func parseSSHLogLine(line string, blacklist map[string]*connectInfo) error {
+func parseSSHLogLine(line string, blacklist map[string]*connectInfo, db *geoip2.Reader) error {
 	if strings.HasPrefix(line, "Failed password for ") {
-		if err := parseSSHFailedPasswordLine(line, blacklist); err != nil {
+		if err := parseSSHFailedPasswordLine(line, blacklist, db); err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
+// exportJSON writes a JSON file version of the blacklist table.
+func exportJSON(blacklist map[string]*connectInfo) {
+	min := float64(math.MaxInt32)
+	max := float64(0)
+	// Find the min and max
+	for _, ip := range blacklist {
+		ipCount := float64(ip.count)
+		if max < ipCount {
+			max = ipCount
+		}
+		if min > ipCount {
+			min = ipCount
+		}
+	}
+	max = max + 1
+	scale := (max - min) / 10
+	buckets := [10]string{}
+	fmt.Printf("Max is %f, Min is %f, scale is %f\n", max, min, scale)
+	// Create dataset "buckets" for the data:
+	for _, ip := range blacklist {
+		bucketNumber := int(math.Floor((float64(ip.count) - min) / scale))
+		if buckets[bucketNumber] != "" {
+			buckets[bucketNumber] += ","
+		}
+		buckets[bucketNumber] += fmt.Sprintf("%f,%f,%d", ip.latitude, ip.longitude, ip.count)
+	}
+	fmt.Println("var data = [")
+	for i, bucketData := range buckets {
+		if i > 0 {
+			fmt.Println(",")
+		}
+		fmt.Printf("['series%d',[%s]]", i, bucketData)
+	}
+	fmt.Println("];")
+}
+
 // readStdIn shows that ideally we are reading a pipe
-func readStdIn(blacklist map[string]*connectInfo) {
+func readStdIn(blacklist map[string]*connectInfo, db *geoip2.Reader) {
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		text, err := reader.ReadString('\n')
 		if err != nil {
-			log.Println("error:", err)
+			if err != io.EOF {
+				log.Println("error:", err)
+			}
+			break
 		}
-		parseSSHLogLine(text, blacklist)
+		parseSSHLogLine(text, blacklist, db)
 	}
 }
 func main() {
-	var blacklist map[string]*connectInfo
-	readStdIn(blacklist)
-	fmt.Println("parser")
+	blacklist := map[string]*connectInfo{}
+	db, err := geoip2.Open("GeoLite2-City.mmdb")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	readStdIn(blacklist, db)
+	exportJSON(blacklist)
 }
--- parser_test.go
+++ parser_test.go
@@ -2,12 +2,31 @@ package main
 
 import (
 	"testing"
+
+	geoip2 "github.com/oschwald/geoip2-golang"
 )
 
 func TestParseSSHFailedPasswordLine(t *testing.T) {
 	blacklist := map[string]*connectInfo{}
-	logLine := "Failed password for root from 59.144.172.42 port 6721 ssh2\n"
-	parseSSHFailedPasswordLine(logLine, blacklist)
+	db, _ := geoip2.Open("GeoLite2-City.mmdb")
+	defer db.Close()
+	lessEntriesLogLine := "Not enough records line\n"
+	errorLessRecords := parseSSHFailedPasswordLine(lessEntriesLogLine, blacklist, db)
+	if errorLessRecords == nil {
+		t.Errorf("Expected errors on less records in Log Line")
+	}
+	moreEntriesLogLine := "Too many records is this line we expect error here\n"
+	errorMoreRecords := parseSSHFailedPasswordLine(moreEntriesLogLine, blacklist, db)
+	if errorMoreRecords == nil {
+		t.Errorf("Expected errors on more records in Log Line")
+	}
+	badIPLogLine := "Failed password for root from X.144.172.42 port 6721 ssh2\n"
+	errorBadIP := parseSSHFailedPasswordLine(badIPLogLine, blacklist, db)
+	if errorBadIP == nil {
+		t.Errorf("Expected errors on Bad IP")
+	}
+	rootLogLine := "Failed password for root from 59.144.172.42 port 6721 ssh2\n"
+	parseSSHFailedPasswordLine(rootLogLine, blacklist, db)
 	ipEntry, ipExists := blacklist["59.144.172.42"]
 	if !ipExists {
 		t.Errorf("IP should have been blacklisted")
@@ -24,14 +43,40 @@ func TestParseSSHFailedPasswordLine(t *testing.T) {
 			}
 		}
 	}
-	parseSSHFailedPasswordLine(logLine, blacklist)
-	parseSSHFailedPasswordLine(logLine, blacklist)
+	parseSSHFailedPasswordLine(rootLogLine, blacklist, db)
+	parseSSHFailedPasswordLine(rootLogLine, blacklist, db)
+	nonRootLogLine := "Failed password for nonroot from 59.144.172.42 port 6721 ssh2\n"
+	parseSSHFailedPasswordLine(nonRootLogLine, blacklist, db)
 	ipEntry, _ = blacklist["59.144.172.42"]
-	if ipEntry.count != 3 {
+	if ipEntry.count != 4 {
 		t.Errorf("IP count not incremented")
 	}
-	userEntry, _ := ipEntry.targetUser["root"]
-	if userEntry != 3 {
-		t.Errorf("User count not incremented")
+	rootUserEntry, _ := ipEntry.targetUser["root"]
+	if rootUserEntry != 3 {
+		t.Errorf("Root user count not incremented")
+	}
+	userEntry, _ := ipEntry.targetUser["nonroot"]
+	if userEntry != 1 {
+		t.Errorf("Non-Root count not incremented")
+	}
+}
+func TestParseSSHLogLine(t *testing.T) {
+	blacklist := map[string]*connectInfo{}
+	db, _ := geoip2.Open("GeoLite2-City.mmdb")
+	defer db.Close()
+	testCases := []struct {
+		line    string
+		isError bool
+	}{
+		{"Failed password for Not enough records line\n", true},
+		{"Failed password for 1 2 3 4 5 6 7 8 9\n", true},
+		{"Failed password for root from 59.144.172.42 port 6721 ssh2\n", false},
+		{"Failed password for nonroot from 59.144.172.42 port 6721 ssh2\n", false},
+	}
+	for _, tc := range testCases {
+		err := parseSSHLogLine(tc.line, blacklist, db)
+		if (err != nil) != tc.isError {
+			t.Errorf("Expected %v", err)
+		}
 	}
 }
